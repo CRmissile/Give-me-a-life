a70feb581f369d789d4fa6a3a0528c76b6abfe5b
CRmissile <yann.cr.missile1@gmail.com>
2020-05-20 17:52:57 +0200

    added mc server

---
+++ start.sh
@@ -0,0 +1,5 @@
+# Minecraft Server startup script using screen
+echo "Starting Minecraft server.  To view window type screen -r minecraft1."
+echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
+cd /home/pi/minecraft1/
+/usr/bin/screen -dmS minecraft1 /usr/bin/java -jar -Xms2600M -Xmx2600M /home/pi/minecraft1/spigot-1.14.4.jar
