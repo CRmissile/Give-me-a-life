afaa48c08111c682344ee56967b79dcd7eebde6d
CRmissile <yann.cr.missile1@gmail.com>
2020-05-20 22:51:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 echo "Starting Minecraft server.  To view window type screen -r minecraft1."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
 cd /home/yann/minecraft1/
-/usr/bin/screen -dmS minecraft1 /usr/bin/java -jar -Xms2600M -Xmx2600M /home/yann/minecraft1/spigot-1.14.4.jar
+/usr/bin/screen -dmS minecraft1 /usr/bin/java -jar -Xms3865M -Xmx3865M /home/yann/minecraft1/spigot-1.14.4.jar
