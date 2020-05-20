eba67bface432ec9159d9d51e1a41a8cc1cb383d
CRmissile <yann.cr.missile1@gmail.com>
2020-05-20 20:56:45 +0200

    Update start_lowspec.sh

--- start_lowspec.sh
+++ start_lowspec.sh
@@ -2,7 +2,7 @@
 # James Chambers - V1.0 - March 24th 2018
 # Marc Tönsing - V1.2 - September 16th 2019
 # Minecraft Server low spec startup script using screen
-echo "Starting Minecraft server.  To view window type screen -r minecraft."
+echo "Starting Minecraft server.  To view window type screen -r minecraft1."
 echo "To minimize the window and let the server run in the background, press Ctrl+A then Ctrl+D"
 cd /home/pi/minecraft1/
 /usr/bin/screen -dmS minecraft1 /usr/bin/java -jar -Xms800M -Xmx800M /home/pi/minecraft1/spigot-1.14.4.jar
